c253bd6d4b4ab2dacf0e858c704329de91173c0c
neil-boardsmith <neil@boardsmith.com>
2025-03-30 08:23:07 +1100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 };
 
 module.exports = nextConfig; 
